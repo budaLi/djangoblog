9936aeeac50dab0f558efdc31fd4787228af055e
152056208 <31475416+152056208@users.noreply.github.com>
2018-08-31 15:04:42 +0800

    Initial commit

---
+++ blog/urls.py
@@ -0,0 +1,16 @@
+#-*- coding:utf8 -*-
+#author : 李不搭
+#date: 2018/8/28
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'blog'
+urlpatterns = [
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.PostDetailView.as_view(), name='detail'),
+    url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.ArchivesView.as_view(), name='archives'),
+    url(r'^category/(?P<pk>[0-9]+)/$', views.CategoryView.as_view(), name='category'),
+    url(r'^tag/(?P<pk>[0-9]+)/$', views.TagView.as_view(), name='tag'),
+    # url(r'^search/$', views.search, name='search'),
+]
